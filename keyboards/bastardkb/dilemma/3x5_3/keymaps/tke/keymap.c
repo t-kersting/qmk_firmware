63653f8c8f3dd5b36a2bd0a87715b2aaa5cb8f07
t-kersting <github@kerst.dev>
2024-02-17 14:49:38 +0100

    - added remaining layer

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/tke/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/tke/keymap.c
@@ -28,13 +28,12 @@
 
 enum dilemma_keymap_layers {
     LAYER_BASE = 0,
-    LAYER_QWERTY,
-    LAYER_MOUSE,
-    LAYER_EXTRA,
     LAYER_NAV,
     LAYER_SYM,
     LAYER_NUM,
     LAYER_FUN,
+    LAYER_MOUSE,
+    LAYER_EXTRA,
 };
 
 enum custom_keycodes {
@@ -53,8 +52,10 @@ enum combos {
 };
 
 // keys
-#define NAV MO(LAYER_NAV)
-#define SYM MO(LAYER_SYM)
+#define NAV LT(LAYER_NAV, KC_SPC)
+#define SYM LT(LAYER_SYM, KC_DEL)
+#define NUM LT(LAYER_NUM, KC_BSPC)
+#define FUN LT(LAYER_FUN, KC_ESC)
 #define MOUSE_X LT(LAYER_MOUSE, DE_Z)
 #define EXT_MIN LT(LAYER_EXTRA, DE_MINS)
 
@@ -102,43 +103,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
        MOUSE_X,    MY_X,    KC_C,    KC_D,    KC_V,       KC_K,    KC_H, KC_COMM,  MY_DOT, EXT_MIN,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                          KC_ESC,  KC_SPC,  KC_TAB,     KC_DEL, KC_BSPC, XXXXXXX
+                             FUN,     NAV,  KC_TAB,     KC_ENT,     NUM,     SYM
   //                   ╰───────────────────────────╯ ╰──────────────────────────╯
   ),
 
   [LAYER_NAV] = LAYOUT_split_3x5_3(
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-        KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,    XXXXXXX, KC_HOME,   KC_UP,  KC_END,  KC_DEL,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,     CUT,    COPY,   PASTE, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LSFT, KC_LCTL, KC_LALT, KC_RGUI, KC_VOLD,    XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
+       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       QK_BOOT,  EE_CLR, KC_MPRV, KC_MNXT, KC_MPLY,    XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,  KC_ENT,
+       XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                            KC_A, _______, KC_LSFT,     KC_SPC, _______,    KC_A
+                         XXXXXXX, XXXXXXX, XXXXXXX,     KC_ENT, KC_BSPC, KC_DEL
   //                   ╰───────────────────────────╯ ╰──────────────────────────╯
   ),
 
   [LAYER_SYM] = LAYOUT_split_3x5_3(
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-        KC_ESC, KC_LBRC, KC_LCBR, KC_LPRN, KC_TILD,    KC_CIRC, KC_RPRN, KC_RCBR, KC_RBRC,  KC_GRV,
+         CARET, DE_SLSH, DE_LCBR, DE_RCBR,   ACUTE,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_MINS, KC_ASTR,  KC_EQL, KC_UNDS,  KC_DLR,    KC_HASH, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT,
+       DE_TILD, DE_HASH, DE_LABK, DE_RABK,   G_ACC,    XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_PLUS, KC_PIPE,   KC_AT, KC_SLSH, KC_PERC,    _______, KC_BSLS, KC_AMPR, KC_QUES, KC_EXLM,
+       DE_CIRC, DE_ACUT, DE_LBRC, DE_RBRC, DE_BSLS,    XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                            KC_A, _______, KC_LSFT,     KC_SPC, _______,    KC_A
+                          KC_DOT,  DE_EQL, KC_COMM,    XXXXXXX, XXXXXXX, XXXXXXX
   //                   ╰───────────────────────────╯ ╰──────────────────────────╯
   ),
 
   [LAYER_NUM] = LAYOUT_split_3x5_3(
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,   KC_0,
+        DE_DEG,    KC_7,    KC_8,    KC_9, DE_EURO,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,  KC_F11,     KC_F12, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT,
+       DE_PIPE,    KC_4,    KC_5,    KC_6, DE_PLUS,    XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
+       DE_BSLS,    KC_1,    KC_2,    KC_3, DE_MINS,    XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                            KC_A, _______, KC_LSFT,     KC_SPC, _______,    KC_A
+                          KC_DOT,    KC_0, KC_COMM,    XXXXXXX, XXXXXXX, XXXXXXX
+  //                   ╰───────────────────────────╯ ╰──────────────────────────╯
+  ),
+
+  [LAYER_FUN] = LAYOUT_split_3x5_3(
+  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_PSCR,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,     KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_SCRL,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,     KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,
+  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
+                         XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  KC_APP, KC_CAPS
   //                   ╰───────────────────────────╯ ╰──────────────────────────╯
   ),
 
@@ -150,19 +163,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
        XXXXXXX,     CUT,    COPY,   PASTE, DRGSCRL,    XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         KC_BTN3, KC_BTN1, KC_BTN2,    XXXXXXX, XXXXXXX, XXXXXXX
+                         KC_BTN3, KC_BTN1, KC_BTN2,    XXXXXXX, KC_BSPC,  KC_DEL
   //                   ╰───────────────────────────╯ ╰──────────────────────────╯
   ),
 
   [LAYER_EXTRA] = LAYOUT_split_3x5_3(
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, RGB_SPI,    XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  KC_ENT, XXXXXXX
+                         KC_MSTP, KC_MPLY, KC_MUTE,    XXXXXXX, XXXXXXX, XXXXXXX
   //                   ╰───────────────────────────╯ ╰──────────────────────────╯
   ),
 };
