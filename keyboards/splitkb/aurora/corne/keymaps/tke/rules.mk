41bdc9c209757748e667d0e00ba068b402113a55
Timo Kersting <tke@fedora.fritz.box>
2024-08-22 22:19:45 +0200

    remove display test added rules

--- keyboards/splitkb/aurora/corne/keymaps/tke/rules.mk
+++ keyboards/splitkb/aurora/corne/keymaps/tke/rules.mk
@@ -1,2 +1,16 @@
 # Convert from Elite-C to Liatris
 CONVERT_TO=liatris
+
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+AUDIO_ENABLE = no           # Audio output
+MAGIC_ENABLE = yes			# Magic keycodes
+TRI_LAYER_ENABLE = yes		# Tri Layers
+CAPS_WORD_ENABLE = yes		# Caps Word
+ENCODER_ENABLE	= yes		# Enable encoder support
+
+COMBO_ENABLE = yes
